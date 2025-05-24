56e99a13ae2cee06df85e01502f359a8d292b4f3
Ayanoo <gagan270903@gmail.com>
2025-05-24 18:06:43 +0530

    Finally add backend folder content

---
+++ backend/helper/hash..js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt')
+
+const hashPassword = async (password) =>{
+    const saltRound = 10
+    const hashedPssword = await bcrypt.hash(password , saltRound)
+    return hashedPssword
+}
+
+const comparePassword = async (password , hashedPssword) => {
+    return bcrypt.compare(password , hashedPssword)
+}
+//hashing message
+
+module.exports = { hashPassword , comparePassword }
